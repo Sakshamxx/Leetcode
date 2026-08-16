393f82ca0f2019457fe23cafb315f3f1e4b793b6
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-08-16 16:04:25 +0530

    LC-maximum-points-you-can-obtain-from-cards [Runtime: N/A; Memory: N/A]

--- prefix-sum/maximum-points-you-can-obtain-from-cards.java
+++ prefix-sum/maximum-points-you-can-obtain-from-cards.java
@@ -1,24 +1,21 @@
 class Solution {
-    public int maxScore(int[] card, int k) {
-     int n = card.length;
-     int j = n-1;   
-     int sum = 0 ;
-     int count = 0;
-     
-
-    for(int i = 0 ; i < k ; i++){
-        sum += card[i];
-    }
-
-    int max = sum;
-
-    while( k > 0){
-        sum -= card[--k];
-        sum +=card[j--];
-        if(sum > max){
-            max = sum;
+    public int maxScore(int[] cardPoints, int k) {
+        int lsum = 0;
+        int rsum = 0;
+        int maxsum = 0;
+        // Grow
+        for (int i =0; i < k;i++){
+            lsum += cardPoints[i];
+            maxsum = lsum;
         }
-    }
-    return max;
+        // Shrink
+        int r_index = cardPoints.length - 1;
+        for (int i =  k -1; i >= 0; i--){
+            lsum -= cardPoints[i];
+            rsum += cardPoints[r_index];
+            r_index--;
+            maxsum = Math.max(maxsum,lsum+rsum);
+        }
+        return maxsum;
     }
 }
