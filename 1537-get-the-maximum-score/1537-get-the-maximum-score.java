cabeae8b90096e1aa3b4254e180ffa4e5bde6e47
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-08-19 22:19:27 +0530

    Time: 4 ms (100%), Space: 61.7 MB (17.44%) - LeetHub

---
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int maxSum(int[] nums1, int[] nums2) {
+
+        int s1 = 0;
+        int i = 0;
+
+        int s2 = 0;
+        int j = 0;
+
+        long ans = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            }
+
+            else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+
+            else {
+
+                long sum1 = 0;
+
+                for (int k = s1; k <= i; k++) {
+                    sum1 += nums1[k];
+                }
+
+                long sum2 = 0;
+
+                for (int k = s2; k <= j; k++) {
+                    sum2 += nums2[k];
+                }
+
+                ans += Math.max(sum1, sum2);
+
+                i++;
+                j++;
+
+                s1 = i;
+                s2 = j;
+            }
+        }
+
+        long sum1 = 0;
+
+        while (s1 < nums1.length) {
+            sum1 += nums1[s1];
+            s1++;
+        }
+
+        long sum2 = 0;
+
+        while (s2 < nums2.length) {
+            sum2 += nums2[s2];
+            s2++;
+        }
+
+        ans += Math.max(sum1, sum2);
+
+        return (int)(ans % 1000000007);
+    }
+}
