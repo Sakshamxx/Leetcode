15bd73f110ae75e9665965977f76ee8a31b7447b
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-08-21 13:56:00 +0530

    LC-get-the-maximum-score [Runtime: N/A; Memory: N/A]

--- two-pointers/get-the-maximum-score.java
+++ two-pointers/get-the-maximum-score.java
@@ -1,47 +1,39 @@
 class Solution {
     public int maxSum(int[] nums1, int[] nums2) {
+
         int i = 0;
-        int s1 = 0;
         int j = 0;
-        int s2 = 0;
+        long sum1 = 0;
+        long sum2 = 0;
         long ans = 0;
 
-        while (i < nums1.length && j < nums2.length){
-            if (nums1[i] < nums2[j]){
-                i++;
-            }
-            else if (nums1[i] > nums2[j]){
-                j++;
-            }else{
-                long sum1 = 0;
-                for (int k = s1; k <= i; k++){
-                    sum1 += nums1[k];
-                }
-
-                long sum2 = 0;
-                for (int k = s2; k<= j; k++){
-                    sum2 += nums2[k];
-                }
-
-                ans+= Math.max(sum1, sum2);
-                i++;
-                j++;
-                s1 = i; 
-                s2 = j;
-                }
-            }
-            long sum1 = 0;
-            while (i < nums1.length){
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
                 sum1 += nums1[i];
                 i++;
-            }
-
-            long sum2 = 0;
-            while ( j < nums2.length){
+            } else if (nums1[i] > nums2[j]) {
                 sum2 += nums2[j];
                 j++;
+
+            } else {
+                ans += Math.max(sum1, sum2);
+                ans += nums1[i];
+                sum1 = 0;
+                sum2 = 0;
+                i++;
+                j++;
             }
-        ans+= Math.max(sum1, sum2);
-        return (int) (ans%1000_000_007);
+        }
+
+        while (i < nums1.length) {
+            sum1 += nums1[i++];
+        }
+
+        while (j < nums2.length) {
+            sum2 += nums2[j++];
+        }
+
+        ans += Math.max(sum1, sum2);
+        return (int) (ans % 1_000_000_007);
     }
 }
