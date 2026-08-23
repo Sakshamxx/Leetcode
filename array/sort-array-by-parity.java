a243a88db41f7d31d06f852edf80b79505fd45f1
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-08-23 16:57:42 +0530

    LC-sort-array-by-parity [Runtime: N/A; Memory: N/A]

---
+++ array/sort-array-by-parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while ( left < right){
+            if (nums[right]%2 != 0 && left < right){
+                right--;
+            }if (nums[left]%2 == 0 && left < right){
+                left ++;
+            }if (nums[left]%2 != 0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return nums;
+    }
+}
