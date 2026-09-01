3922856977d2124da615e1efc3085e880fdf4328
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-09-01 08:34:30 +0530

    LC-largest-number [Runtime: N/A; Memory: N/A]

---
+++ greedy/largest-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+
+        for (int i = 0; i <= nums.length - 1; i++){
+            arr[i] = nums[i]+"";
+        }
+        Arrays.sort(arr, (s1,s2)->(s1+s2).compareTo(s2+s1));
+
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = arr.length - 1; i >= 0; i--){
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+        
+    }
+}
