2ad81d4b0722214d4bb6e3d61c39f0bc2e609c07
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-09-20 08:06:13 +0530

    LC-reverse-degree-of-a-string [Runtime: N/A; Memory: N/A]

---
+++ simulation/reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            int alphabet = ch - 'a' + 1;
+            int reverse = 27 - alphabet;
+            int product = (i+1)*reverse;
+            sum+=product;
+        }
+        return sum;
+    }
+}
